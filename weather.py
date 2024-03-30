25417ad935937813d10a688d13e75e0377ea4c5d
olliebowe <93463703+olliebowe@users.noreply.github.com>
2024-03-31 00:35:51 +1000

    json added

--- weather.py
+++ weather.py
@@ -10,9 +10,13 @@ load_dotenv()
 api_key = os.getenv("AMBIENT_WEATHER_API_KEY")
 application_key = os.getenv("AMBIENT_WEATHER_APPLICATION_KEY")
 
-
-
 url = f"https://api.ambientweather.net/v1/devices?apiKey={api_key}&applicationKey={application_key}"
 
 response = requests.get(url)
-print(response.text)
+
+# Parse the response to JSON
+data = response.json()
+
+# Use json.dumps to pretty-print the JSON data
+pretty_json = json.dumps(data, indent=4)
+print(pretty_json)
